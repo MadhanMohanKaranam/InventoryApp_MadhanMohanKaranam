58a4ba8e8e2d8b93b0f786f04607efea5b433fa1
MadhanMohanKaranam <mkaranam@uco.edu>
2024-01-26 00:51:03 -0600

    added button

--- public/app.js
+++ public/app.js
@@ -1,13 +1,20 @@
 const b1 = document.getElementById('buttonNumber');
 const b2 = document.getElementById('buttonName');
+const rootEl = document.getElementById('root');
 
 b1.onclick = f1;
 b2.onclick = function() {
-    console.log('Name button is clicked');
+    rootEl.innerHTML = 'my name is <b>madhan</b>';
 }
 
 function f1() {
-    console.log('Number button clicked');
+    const r1 = Math.random();
+    const n1 = Math.floor(r1 * 100);
+    const r2 = Math.random();
+    const n2 = Math.floor(r2 * 100);
+
+    rootEl.innerHTML = 'your lucky numbers are ' + n1 + ' and ' + n2;
+
 }
 
 
