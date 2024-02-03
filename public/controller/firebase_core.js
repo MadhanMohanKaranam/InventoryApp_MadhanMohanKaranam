b46c41c9f539c52d0dd7f57ce36ed8824505bf0c
MadhanMohanKaranam <mkaranam@uco.edu>
2024-02-03 15:46:07 -0600

    final commit

--- public/controller/firebase_core.js
+++ public/controller/firebase_core.js
@@ -1,18 +1,18 @@
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCzG4k1PqD_0D3qIbt-h6bS2z359MXOYCM",
-    authDomain: "gchunduru-cmsc5373-cloud.firebaseapp.com",
-    projectId: "gchunduru-cmsc5373-cloud",
-    storageBucket: "gchunduru-cmsc5373-cloud.appspot.com",
-    messagingSenderId: "1085289468899",
-    appId: "1:1085289468899:web:37b45f439421e7191b86d6"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCmzTURrgDXLKe_iRj9dwW96szwJGt-pQE",
+    authDomain: "mkaranam-cmsc5373-webapp.firebaseapp.com",
+    projectId: "mkaranam-cmsc5373-webapp",
+    storageBucket: "mkaranam-cmsc5373-webapp.appspot.com",
+    messagingSenderId: "295730971981",
+    appId: "1:295730971981:web:19de776105775b8f22a304"
+  };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
