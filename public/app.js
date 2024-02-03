aaac7a57878c8c20730bd7158d10b9f4cd78abd5
MadhanMohanKaranam <mkaranam@uco.edu>
2024-02-03 15:08:09 -0600

    final commit

--- public/app.js
+++ public/app.js
@@ -1,20 +1,24 @@
-const b1 = document.getElementById('buttonNumber');
-const b2 = document.getElementById('buttonName');
-const rootEl = document.getElementById('root');
+import { onClickHomeMenu, onClickMenu2Menu, onClickSignoutMenu } from "./controller/menueventhandlers.js";
+import { attachAuthStateChangeObserver } from "./controller/firebase_auth.js";
+import { routing } from "./controller/route_controller.js";
 
-b1.onclick = f1;
-b2.onclick = function() {
-    rootEl.innerHTML = 'my name is <b>madhan</b>';
+document.getElementById('menu-home').onclick = onClickHomeMenu;
+document.getElementById('menu-menu2').onclick = onClickMenu2Menu;
+document.getElementById('menu-signout').onclick = onClickSignoutMenu;
+
+attachAuthStateChangeObserver();
+
+window.onload = function(e){
+    const pathname = window.location.pathname;
+    const hash = window.location.hash;
+    console.log(pathname,hash);
+    routing(pathname,hash);
 }
 
-function f1() {
-    const r1 = Math.random();
-    const n1 = Math.floor(r1 * 100);
-    const r2 = Math.random();
-    const n2 = Math.floor(r2 * 100);
-
-    rootEl.innerHTML = 'your lucky numbers are ' + n1 + ' and ' + n2;
-
+window.onpopstate = function(e){
+    e.preventDefault();
+    const pathname = window.location.pathname;
+    const hash = window.location.hash;
+    routing(pathname, hash);
 }
-
 
---
+++ public/controller/firebase_core.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCzG4k1PqD_0D3qIbt-h6bS2z359MXOYCM",
+    authDomain: "gchunduru-cmsc5373-cloud.firebaseapp.com",
+    projectId: "gchunduru-cmsc5373-cloud",
+    storageBucket: "gchunduru-cmsc5373-cloud.appspot.com",
+    messagingSenderId: "1085289468899",
+    appId: "1:1085289468899:web:37b45f439421e7191b86d6"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
